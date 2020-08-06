e22e22dedd5a676ba1602e2b3fffaa2303b6bb5a
Subash Maharjan <hudeomah@gmail.com>
2020-08-05 20:19:38 -0400

    constants created

---
+++ src/store/modules/enums.js
@@ -0,0 +1,13 @@
+export const COMPANY = {
+    ENVIRO: 'Enviroapps', 
+    SILVERCHAIR: 'Silverchair', 
+    DMI: 'Digital Management', 
+    UPDATER: 'Vertical One', 
+    SABRE: 'Sabre Systems', 
+    BODHI: 'Bodhi Systems'
+}
+
+export const CATEGORY = {
+    WEB: 'Web Development',
+    MOBILE: 'Mobile Development',
+}
