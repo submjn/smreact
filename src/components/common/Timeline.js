faf1d4593f28ad828e6fc23e9af8050c9adaf31b
Subash Maharjan <hudeomah@gmail.com>
2020-08-05 20:20:25 -0400

    data separated from component

--- src/components/common/Timeline.js
+++ src/components/common/Timeline.js
@@ -1,76 +1,25 @@
 import React, { Component } from 'react'
 
-const timelineData = [
-    {
-        id: 1,
-        timeframe: {
-            start: 'Nov 2018',
-            end: 'Present'
-        },
-        company: 'Sabre Systems',
-        position: 'Senior Software Developer',
-        location: 'Remote (Charlottesville, Virginia)',
-        description: 'Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.'
-    },
-    {
-        id: 2,
-        timeframe: {
-            start: 'March 2018',
-            end: 'November 2018'
-        },
-        company: 'Vertical One',
-        position: 'Senior Frontend Engineer',
-        location: 'Charlottesville, Virginia',
-        description: 'Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.'
-    },
-    {
-        id: 3,
-        timeframe: {
-            start: 'October 2015',
-            end: 'March 2018'
-        },
-        company: 'Digital Management Inc',
-        position: 'Senior Frontend Developer',
-        location: 'Remote (Charlottesville, Virginia)',
-        description: 'Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.'
-    },
-    {
-        id: 4,
-        timeframe: {
-            start: 'September 2010',
-            end: 'October 2015'
-        },
-        company: 'Silverchair Inc',
-        position: 'UI Developer',
-        location: 'Charlottesville, Virginia',
-        description: 'Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.'
-    },
-    {
-      id: 5,
-      timeframe: {
-          start: 'October 2009',
-          end: 'September 2010'
-      },
-      company: 'Enviroapps',
-      position: 'UI Developer',
-      location: 'Orlando, Florida',
-      description: 'Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.'
-  },
-]
+import { timelineData } from '../../store/data/timeline'
 
 const Timeline = ({item}) => (
     <div className="timeline">
         {
             item.map(item => (
+
               <div className="timeline__block" key={item.id}>
                   <div className="timeline__bullet"></div>
                   <div className="timeline__header">
                       <p className="timeline__timeframe">{item.timeframe.start} - {item.timeframe.end}</p>
-                      <h3>{item.company}</h3>
+                      <h3><a href={item.link}>{item.company}</a></h3>
                       <h5>{item.position}</h5>
                   </div>
                   <div className="timeline__desc">
                       <p>{item.description}</p>
+                      
+                      {
+                        item.highlights ? <p className="small"><strong>Highlights: </strong> {item.highlights}</p> : ''
+                      }
                   </div>
               </div>
             ))
